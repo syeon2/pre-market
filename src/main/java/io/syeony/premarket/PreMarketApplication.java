abe577c3a42c3e282a432a4665be6abd8d49dc75
SUYEON KIM <waterkite94@gmail.com>
2024-10-08 23:09:12 +0900

    Init Project

---
+++ src/main/java/io/syeony/premarket/PreMarketApplication.java
@@ -0,0 +1,13 @@
+package io.syeony.premarket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PreMarketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PreMarketApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/syeony/premarket/PreMarketApplicationTests.java
@@ -0,0 +1,13 @@
+package io.syeony.premarket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PreMarketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
