f968ee3d9add2a949be9cb4c3e47b664ed2770b0
SUYEON KIM <waterkite94@gmail.com>
2024-10-09 00:35:46 +0900

    chore : update checkstyle and build.gradle

--- src/main/java/io/syeony/premarket/PreMarketApplication.java
+++ src/main/java/io/syeony/premarket/PreMarketApplication.java
@@ -6,8 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class PreMarketApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(PreMarketApplication.class, args);
-    }
-
+	public static void main(String[] args) {
+		SpringApplication.run(PreMarketApplication.class, args);
+	}
 }
