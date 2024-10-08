e378c34bc83164dae86ef58f5f6c5466d1d1ab51
SUYEON KIM <waterkite94@gmail.com>
2024-10-08 23:42:49 +0900

    chore : add checkstyle
    Add: checkstyle files, formatter file

--- src/main/java/io/syeony/premarket/PreMarketApplication.java
+++ src/main/java/io/syeony/premarket/PreMarketApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class PreMarketApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(PreMarketApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(PreMarketApplication.class, args);
+    }
 
 }
